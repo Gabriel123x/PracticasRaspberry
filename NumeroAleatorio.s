2029fdb361ccb3c7618266cc67fc6695642869cb
Gabriel123x <43393807+Gabriel123x@users.noreply.github.com>
2020-02-25 13:37:25 -0800

    Add files via upload

---
+++ NumeroAleatorio.s
@@ -0,0 +1,76 @@
+@----------------------------------
+
+.data
+
+@ See /usr/include/arm-linux-gnueabihf/asm/unistd.h
+@ See /usr/include/arm-linux-gnueabihf/bits/fcntl-linux.h
+
+    .equ create,     8
+         .equ Mode, 0644       @ -rw-r--r--
+    .equ open,       5
+         .equ Rd,   00
+         .equ Wr,   01
+         .equ RdWr, 02
+         .equ Apnd, 02000
+    .equ read,       3
+    .equ write,      4
+    .equ close,      6
+    .equ sync,       36
+    .equ exit,       1
+    .equ sfile,      187
+
+@----------------------------------
+
+.balign 4
+dir_file:
+    .asciz "/dev/urandom"
+
+.balign 4
+Open:
+    .word dir_file, RdWr | Apnd, open
+
+.balign 4
+Buf:
+    .word 0 
+
+.balign 4
+Read:
+    .word Buf, 1, read
+
+.balign 4
+format:
+    .asciz "%3d\n"
+
+@----------------------------------
+
+.text
+
+.global main, printf
+
+main:
+
+     push   {r4, r5, r7, lr}     @ folowing AAPCS
+
+     ldr    r3, =Open            @ load address
+     ldm    r3, {r0, r1, r7}     @ load registers
+     svc    #0                   @ OS opens file
+     mov    r4, r0               @ save fd in r4
+
+     ldr    r3, =Read            @ load address
+     ldm    r3, {r1, r2, r7}     @ load registers
+     svc    #0                   @ OS reads file
+
+     mov    r0, r4               @ move fd in r0
+     mov    r7, #close           @ num for close
+     svc    #0                   @ OS closes file
+
+     ldr    r0, =format          @ adress of format
+     ldr    r1, =Buf             @ addr of byte red
+     ldr    r1, [r1]             @ load byte
+     bl     printf               @ C() print byte
+     mov    r0, #0               @ 0 = success
+
+exit:
+
+     pop    {r4, r5, r7, lr}     @ folowing AAPCS
+     bx     lr                   @ Exit if use gcc as linkerran
