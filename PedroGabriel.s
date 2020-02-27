a1e7ea0992e29a949197c8c9ce0e10bd52c24fdf
Gabriel123x <43393807+Gabriel123x@users.noreply.github.com>
2020-02-27 14:14:46 -0800

    Add files via upload

---
+++ PedroGabriel.s
@@ -0,0 +1,18 @@
+
+.data
+
+cadena: .asciz "Hola  Soy Pedro!\n"
+cadenafin:
+
+.text
+.global  main
+
+main:    PUSH    {r7, lr} 
+         MOV      r0, #1 
+         LDR      r1, =cadena 
+         MOV      r2, #cadenafin -cadena 
+         MOV      r7, #4  
+         SWI      #0
+         MOV      r0, #0 
+         POP      {r7, lr}  
+         BX       lr
